403c0b9a22808b2132747fe6e0a546336543ca4f
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-06 23:07:22 -0500

    props destructuring/children

--- src/index.js
+++ src/index.js
@@ -6,23 +6,37 @@ const bookOne = {
   title: 'The Four Agreements: A Practical Guide to Personal Freedom',
   author: 'Don Miguel Ruiz',
   img:
-    'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg',
+    'https://images-na.ssl-images-amazon.com/images/I/51FcSSKpJBL._SX343_BO1,204,203,200_.jpg',
+};
+const bookTwo = {
+  title: 'The Vanishing Half',
+  author: 'Brit Bennett',
+  img: 'https://m.media-amazon.com/images/I/41ThX3SfAiL.jpg',
 };
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={bookOne.img} title={bookOne.title} author={bookOne.author} />
+      <Book img={bookOne.img} title={bookOne.title} author={bookOne.author}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+          voluptates.
+        </p>
+      </Book>
+      <Book img={bookTwo.img} title={bookTwo.title} author={bookTwo.author} />
     </section>
   );
 }
 
 const Book = (props) => {
+  const { img, title, author } = props;
+
   return (
     <article className='book'>
-      <img src={props.img} alt='' />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {props.children}
     </article>
   );
 };
