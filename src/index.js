e5af9adb1ee1756fb4fa9b50bb341396a0ec60c1
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-06 23:32:59 -0500

    proper lists

--- src/index.js
+++ src/index.js
@@ -2,41 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const bookOne = {
-  title: 'The Four Agreements: A Practical Guide to Personal Freedom',
-  author: 'Don Miguel Ruiz',
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/51FcSSKpJBL._SX343_BO1,204,203,200_.jpg',
-};
-const bookTwo = {
-  title: 'The Vanishing Half',
-  author: 'Brit Bennett',
-  img: 'https://m.media-amazon.com/images/I/41ThX3SfAiL.jpg',
-};
+const books = [
+  {
+    title: 'The Four Agreements: A Practical Guide to Personal Freedom',
+    author: 'Don Miguel Ruiz',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/51FcSSKpJBL._SX343_BO1,204,203,200_.jpg',
+  },
+  {
+    title: 'The Vanishing Half',
+    author: 'Brit Bennett',
+    img: 'https://m.media-amazon.com/images/I/41ThX3SfAiL.jpg',
+  },
+  {
+    title:
+      'Lead from the Outside: How to Build Your Future and Make Real Change',
+    author: 'Stacey Abrams',
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/41L+6OcWobL._SX324_BO1,204,203,200_.jpg',
+  },
+];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={bookOne.img} title={bookOne.title} author={bookOne.author}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
-          voluptates.
-        </p>
-      </Book>
-      <Book img={bookTwo.img} title={bookTwo.title} author={bookTwo.author} />
+      {books.map((book) => (
+        <Book book={book} />
+      ))}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
 
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
